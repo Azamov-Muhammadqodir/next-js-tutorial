ae98bdf684326892f5badfffa23df2097675a511
Azamov-Muhammadqodir <azamovm323@gmail.com>
2024-06-10 21:29:57 +0500

    NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
+import "nprogress/nprogress.css";
+import "@/styles/globals.css"; 
 import createEmotionCache from "@/helpers/create-emotion-cache";
 import theme from "@/helpers/theme";
-import "@/styles/globals.css";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import { Roboto } from 'next/font/google'
 import Head from "next/head";
+import NProgress from "nprogress";
+import { useEffect } from "react";
+import Router from "next/router";
 const roboto = Roboto({
   weight: ['300','400','500', '700'],
   style: ['normal', 'italic'],
@@ -21,6 +25,22 @@ export interface MyAppProps extends AppProps{
 
 export default function App(props: MyAppProps) {
   const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+ 
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+ 
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
